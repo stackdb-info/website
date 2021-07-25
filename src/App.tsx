ec5088671a5c1561befa47bbe39f98d0cc825d64
Léo Rolland <rolland.leo@orange.fr>
2021-07-25 18:02:10 +0200

    add: types list

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,38 @@ import { gql } from 'apollo-boost';
 import * as React from 'react';
 import { Query } from 'react-apollo';
 
-const LOCAL_HELLO = gql`
-  query localHello($subject: String) {
-    localHello(subject: $subject) @client
+const TYPES_LIST = gql`
+  query {
+    queryTypesList {
+      types
+    }
   }
 `;
 
-const SERVER_HELLO = gql`
-  query serverHello($subject: String) {
-    hello(subject: $subject)
-  }
-`;
+const TypesList = () => (
+  <div>
+    <h2>Types List</h2>
+    <Query query={TYPES_LIST}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return 'Loading...';
+        }
 
-const LocalHello = () => (
-  <Query query={LOCAL_HELLO} variables={{ subject: 'World' }}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return 'Loading...';
-      }
-
-      return <h2>Local Salutation: {error ? error.message : data.localHello}</h2>;
-    }}
-  </Query>
-);
-
-const ServerHello = () => (
-  <Query query={SERVER_HELLO} variables={{ subject: 'World' }}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return 'Loading...';
-      }
-
-      return (
-        <h2>
-          Server Salutation:&nbsp;
-          {error
-            ? error.message + '. You probably don`t have GraphQL Server running at the moment - thats okay'
-            : data.hello}
-        </h2>
-      );
-    }}
-  </Query>
+        return (
+          <h2>
+            {error
+              ? error.message + '. You probably don`t have GraphQL Server running at the moment - thats okay'
+              : JSON.stringify(data.queryTypesList[0].types)}
+          </h2>
+        );
+      }}
+    </Query>  
+  </div>
 );
 
 const App = () => (
   <div>
-    <h1>
-      Welcome to your own <a href="http://localhost:8080/graphiql">GraphQL</a> web front end!
-    </h1>
-    <h2>You can start editing source code and see results immediately</h2>
-    <LocalHello />
-    <ServerHello />
+    <TypesList />
   </div>
 );
 
