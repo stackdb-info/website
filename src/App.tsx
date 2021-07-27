5805030c7fad6e8e02030c4c62247215ef897a44
Léo <rolland.leo@orange.fr>
2021-07-27 20:50:35 +0200

    add: basic routing and queries

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { gql } from 'apollo-boost';
 import * as React from 'react';
 import { Query } from 'react-apollo';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
 const TYPES_LIST = gql`
   query {
     queryTypesList {
@@ -9,32 +17,156 @@ const TYPES_LIST = gql`
     }
   }
 `;
-
 const TypesList = () => (
   <div>
     <h2>Types List</h2>
     <Query query={TYPES_LIST}>
       {({ loading, error, data }) => {
-        if (loading) {
-          return 'Loading...';
-        }
-
+        if (loading) return 'Loading...'
+        if (error) return 'Unable to connect to GraphQL api : ' + error.message
+        const types = data.queryTypesList[0].types
+        let liTypes = types.map(type =>
+          <li key={type}>
+            <Link to={`/${encodeURIComponent(type)}`}>{type}</Link>
+          </li>
+        )
         return (
-          <h2>
-            {error
-              ? error.message + '. You probably don`t have GraphQL Server running at the moment - thats okay'
-              : JSON.stringify(data.queryTypesList[0].types)}
-          </h2>
+          <div>
+            <ul>{liTypes}</ul>
+          </div>
         );
       }}
-    </Query>  
+    </Query>
   </div>
 );
 
-const App = () => (
-  <div>
-    <TypesList />
-  </div>
-);
+const BY_TYPES = (type, fields) => gql`
+  query {
+    query${type} {
+      ${fields.join('\n')}
+    }
+  }
+`
+const INTRO_TYPE = type => gql`
+  query {
+    __type(name: "${type}") {
+       fields {
+          name
+          type {
+            kind
+            ofType {
+              name
+              kind  
+            }
+          }
+       }  
+    }
+  }
+`
+const TypePage = () => {
+  let { type } = useParams();
+  return (
+    <div>
+      <h1>{type}</h1>
+      <Query query={INTRO_TYPE(type)}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return 'Unable to connect to GraphQL api : ' + error.message
+          const fields = data.__type.fields
+          let parameters = fields
+            .filter(f => f.type.ofType && f.type.ofType.name == 'String')
+            .map(f => f.name)
+          return (
+            <Query query={BY_TYPES(type, parameters)}>
+              {({ loading, error, data }) => {
+                if (loading) return 'Loading...'
+                if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                const items = data["query" + type]
+                let liItems = items.map(i =>
+                  <li key={i.name}>
+                    <Link to={`/${encodeURIComponent(type)}/${encodeURIComponent(i.name)}`}>
+                      {i.name}
+                    </Link>
+                  </li>
+                )
+                return (
+                  <ul>{liItems}</ul>
+                )
+              }}
+            </Query>
+          )
+        }}
+      </Query>
+    </div >
+  )
+}
+
+const GET = (type, fields, name) => gql`
+  query {
+    get${type}${name ? '(name: "' + name + '")' : ''} {
+      ${fields.join('\n')}
+    }
+  }
+`
+const TechnoPage = () => {
+  let { type, techno } = useParams();
+  return (
+    <div>
+      <h1>{techno}</h1>
+      <Query query={INTRO_TYPE(type)}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return 'Unable to connect to GraphQL api : ' + error.message
+          const fields = data.__type.fields
+          let parameters = fields
+            .filter(f => !(f.name as string).endsWith('Aggregate'))
+            .map(f => {
+              // TODO : refactor for cleaner and more explicit condition
+              if (!f.type.ofType || (f.type.ofType && f.type.ofType.name == 'String'))
+                return f.name
+              return `${f.name} { name }`
+
+            })
+          return (
+            <Query query={GET(type, parameters, techno)}>
+              {({ loading, error, data }) => {
+                if (loading) return 'Loading...'
+                if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                const items = data["get" + type]
+                let liItems = Object.keys(items).map(key =>
+                  <li key={key}>
+                    {key} : {JSON.stringify(items[key])}
+                  </li>
+                )
+                return (
+                  <ul>{liItems}</ul>
+                )
+              }}
+            </Query>
+          )
+        }}
+      </Query>
+    </div >
+  )
+}
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/:type/:techno">
+            <TechnoPage />
+          </Route>
+          <Route path="/:type">
+            <TypePage />
+          </Route>
+          <Route path="/">
+            <TypesList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>)
+}
 
 export default App;
