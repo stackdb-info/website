4880e2eaa78c0cfdeb10e651ea8045828b146722
Léo <rolland.leo@orange.fr>
2021-07-28 11:51:58 +0200

    fix: br closing

--- src/App.tsx
+++ src/App.tsx
@@ -117,60 +117,60 @@ const TechnoPage = () => {
   return (
     <div>
       <h1>{capitalize(techno)}</h1>
-      <Link to={`/`}>Show all categories</Link><br>
-        <Link to={`/${type}`}>Show all {plural(type)}</Link>
-        <Query query={INTRO_TYPE(type)}>
-          {({ loading, error, data }) => {
-            if (loading) return 'Loading...'
-            if (error) return 'Unable to connect to GraphQL api : ' + error.message
-            const fields: any[] = data.__type.fields
-            let parameters = fields
-              .filter(f => !(f.name as string).endsWith('Aggregate'))
-              .map(f => {
-                // TODO : refactor for cleaner and more explicit condition
-                if (!f.type.ofType || (f.type.ofType && f.type.ofType.name == 'String'))
-                  return f.name
-                return `${f.name} { name }`
-              })
-            return (
-              <Query query={GET(type, parameters, techno)}>
-                {({ loading, error, data }) => {
-                  if (loading) return 'Loading...'
-                  if (error) return 'Unable to connect to GraphQL api : ' + error.message
-                  const items = data["get" + capitalize(type)]
-                  console.log(items)
-                  let liItems = Object.keys(items).filter(k => k != '__typename').map(key =>
-                    <li key={key}>
-                      <b>{key} :</b> <Field fields={fields} name={key} val={items[key]} />
-                    </li>
-                  )
-                  return <ul>{liItems}</ul>
-                }}
-              </Query>
-            )
-          }}
-        </Query>
+      <Link to={`/`}>Show all categories</Link><br />
+      <Link to={`/${type}`}>Show all {plural(type)}</Link>
+      <Query query={INTRO_TYPE(type)}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return 'Unable to connect to GraphQL api : ' + error.message
+          const fields: any[] = data.__type.fields
+          let parameters = fields
+            .filter(f => !(f.name as string).endsWith('Aggregate'))
+            .map(f => {
+              // TODO : refactor for cleaner and more explicit condition
+              if (!f.type.ofType || (f.type.ofType && f.type.ofType.name == 'String'))
+                return f.name
+              return `${f.name} { name }`
+            })
+          return (
+            <Query query={GET(type, parameters, techno)}>
+              {({ loading, error, data }) => {
+                if (loading) return 'Loading...'
+                if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                const items = data["get" + capitalize(type)]
+                console.log(items)
+                let liItems = Object.keys(items).filter(k => k != '__typename').map(key =>
+                  <li key={key}>
+                    <b>{key} :</b> <Field fields={fields} name={key} val={items[key]} />
+                  </li>
+                )
+                return <ul>{liItems}</ul>
+              }}
+            </Query>
+          )
+        }}
+      </Query>
     </div >
-      )
+  )
 }
 
 const App = () => {
   return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/:type/:techno">
-              <TechnoPage />
-            </Route>
-            <Route path="/:type">
-              <TypePage />
-            </Route>
-            <Route path="/">
-              <TypesList />
-            </Route>
-          </Switch>
-        </div>
-      </Router>)
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/:type/:techno">
+            <TechnoPage />
+          </Route>
+          <Route path="/:type">
+            <TypePage />
+          </Route>
+          <Route path="/">
+            <TypesList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>)
 }
 
-      export default App;
+export default App;
