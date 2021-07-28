bcef5df363011e4cd945117532a6cbe86ab041cb
Léo <rolland.leo@orange.fr>
2021-07-28 11:36:08 +0200

    add: better navigation, capitalize, plural

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 } from "react-router-dom";
 
 import Field from './Field';
-import { capitalize } from './tools';
+import { capitalize, plural } from './tools';
 
 const TYPES_LIST = gql`
   query {
@@ -22,7 +22,7 @@ const TYPES_LIST = gql`
 `;
 const TypesList = () => (
   <div>
-    <h2>Types List</h2>
+    <h1>Categories</h1>
     <Query query={TYPES_LIST}>
       {({ loading, error, data }) => {
         if (loading) return 'Loading...'
@@ -30,7 +30,7 @@ const TypesList = () => (
         const types = data.queryTypesList[0].types
         let liTypes = types.map(type =>
           <li key={type}>
-            <Link to={`/${encodeURIComponent(type)}`}>{type}</Link>
+            <Link to={`/${encodeURIComponent(type)}`}>{plural(capitalize(type))}</Link>
           </li>
         )
         return (
@@ -70,7 +70,8 @@ const TypePage = () => {
   let { type } = useParams();
   return (
     <div>
-      <h1>{capitalize(type)}</h1>
+      <h1>{plural(capitalize(type))}</h1>
+      <Link to={`/`}>Show all categories</Link>
       <Query query={INTRO_TYPE(type)}>
         {({ loading, error, data }) => {
           if (loading) return 'Loading...'
@@ -116,58 +117,60 @@ const TechnoPage = () => {
   return (
     <div>
       <h1>{capitalize(techno)}</h1>
-      <Query query={INTRO_TYPE(type)}>
-        {({ loading, error, data }) => {
-          if (loading) return 'Loading...'
-          if (error) return 'Unable to connect to GraphQL api : ' + error.message
-          const fields: any[] = data.__type.fields
-          let parameters = fields
-            .filter(f => !(f.name as string).endsWith('Aggregate'))
-            .map(f => {
-              // TODO : refactor for cleaner and more explicit condition
-              if (!f.type.ofType || (f.type.ofType && f.type.ofType.name == 'String'))
-                return f.name
-              return `${f.name} { name }`
-            })
-          return (
-            <Query query={GET(type, parameters, techno)}>
-              {({ loading, error, data }) => {
-                if (loading) return 'Loading...'
-                if (error) return 'Unable to connect to GraphQL api : ' + error.message
-                const items = data["get" + capitalize(type)]
-                console.log(items)
-                let liItems = Object.keys(items).filter(k => k != '__typename').map(key =>
-                  <li key={key}>
-                    <b>{key} :</b> <Field fields={fields} name={key} val={items[key]} />
-                  </li>
-                )
-                return <ul>{liItems}</ul>
-              }}
-            </Query>
-          )
-        }}
-      </Query>
+      <Link to={`/`}>Show all categories</Link><br>
+        <Link to={`/${type}`}>Show all {plural(type)}</Link>
+        <Query query={INTRO_TYPE(type)}>
+          {({ loading, error, data }) => {
+            if (loading) return 'Loading...'
+            if (error) return 'Unable to connect to GraphQL api : ' + error.message
+            const fields: any[] = data.__type.fields
+            let parameters = fields
+              .filter(f => !(f.name as string).endsWith('Aggregate'))
+              .map(f => {
+                // TODO : refactor for cleaner and more explicit condition
+                if (!f.type.ofType || (f.type.ofType && f.type.ofType.name == 'String'))
+                  return f.name
+                return `${f.name} { name }`
+              })
+            return (
+              <Query query={GET(type, parameters, techno)}>
+                {({ loading, error, data }) => {
+                  if (loading) return 'Loading...'
+                  if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                  const items = data["get" + capitalize(type)]
+                  console.log(items)
+                  let liItems = Object.keys(items).filter(k => k != '__typename').map(key =>
+                    <li key={key}>
+                      <b>{key} :</b> <Field fields={fields} name={key} val={items[key]} />
+                    </li>
+                  )
+                  return <ul>{liItems}</ul>
+                }}
+              </Query>
+            )
+          }}
+        </Query>
     </div >
-  )
+      )
 }
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/:type/:techno">
-            <TechnoPage />
-          </Route>
-          <Route path="/:type">
-            <TypePage />
-          </Route>
-          <Route path="/">
-            <TypesList />
-          </Route>
-        </Switch>
-      </div>
-    </Router>)
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/:type/:techno">
+              <TechnoPage />
+            </Route>
+            <Route path="/:type">
+              <TypePage />
+            </Route>
+            <Route path="/">
+              <TypesList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>)
 }
 
-export default App;
+      export default App;
--- src/tools.tsx
+++ src/tools.tsx
@@ -1,3 +1,7 @@
 export function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+export function plural(string: string) {
+    if (string.endsWith('s')) return string
+    return string + 's'
+}
