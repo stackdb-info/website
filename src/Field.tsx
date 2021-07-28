710f61438684281ba2570e5fe4defc903c0b8141
Léo <rolland.leo@orange.fr>
2021-07-28 12:09:46 +0200

    fix: upper char in url

--- src/Field.tsx
+++ src/Field.tsx
@@ -5,7 +5,10 @@ const Field = props => {
     const { fields, name, val } = props
     console.log(fields, name, val)
     let dataType = fields.find(f => f.name == name).type
-    if (dataType.kind == 'LIST') return val.map(val => <Link key={val.name} to={`/${val.__typename}/${val.name}`}>{val.name}</Link>)
+    if (dataType.kind == 'LIST') {
+        let lis = val.map(val => <li><Link key={val.name} to={`/${val.__typename.toLowerCase()}/${val.name}`}>{val.name}</Link></li>)
+        return <ul>{lis}</ul>
+    }
     if (!dataType.ofType || dataType.ofType.name == 'String') return val
     return JSON.stringify(val)
 }
