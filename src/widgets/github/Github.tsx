03473968e79837ff0112d096114ca06d3c32ea3d
Léo <rolland.leo@orange.fr>
2021-07-29 20:42:11 +0200

    fix: bad key filter github component

--- src/widgets/github/Github.tsx
+++ src/widgets/github/Github.tsx
@@ -8,7 +8,7 @@ export default function Github(params) {
     if (Object.keys(params.items).includes("github_repository"))
         return <Repository repository={params.items.github_repository} />
 
-    if (Object.keys(params.items).includes("github_repository"))
+    if (Object.keys(params.items).includes("github_organization"))
         return <Organization organization={params.items.github_organization} />
 
     return null
