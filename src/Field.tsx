b13c5c4c2abf94e12c8dd9117e539aaf67070fff
Léo Rolland <rolland.leo@orange.fr>
2021-08-01 15:26:48 +0200

    add: kind LIST are recrusively invoking Field

--- src/Field.tsx
+++ src/Field.tsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 
 const Field = props => {
     const { fields, name, val } = props
+    console.log(props)
     let dataType = fields.find(f => f.name == name).type
     if (dataType.kind == 'LIST') {
-        let lis = val.map(val => <li><Link key={val.name} to={`/${val.__typename.toLowerCase()}/${val.name}`}>{val.name}</Link></li>)
+        let lis = val.map(val => <li><Field fields={[{name: name, type: dataType.ofType}]} name={name} val={val}/></li>)
         return <ul>{lis}</ul>
+    }
+    if (dataType.kind == 'OBJECT') {
+        return <Link key={val.name} to={`/${val.__typename.toLowerCase()}/${val.name}`}>{val.name}</Link>
     }
     if (!dataType.ofType || dataType.ofType.name == 'String') return val
     return JSON.stringify(val)
