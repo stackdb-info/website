379da7303029981504dab24ce74211c1a58a29de
leo <leo@vps462003.ovh.net>
2021-09-20 21:26:48 +0200

    wip : basic style in techno

--- src/pages/TechnoPage.tsx
+++ src/pages/TechnoPage.tsx
@@ -9,6 +9,7 @@ import { GET, INTRO_TYPE } from '../queries';
 import Github from '../widgets/github/Github';
 import GoogleTrends from '../widgets/GoogleTrends';
 import Field from '../Field';
+import './style.css'
 
 export const TechnoPage = () => {
     let { type, techno } = useParams()
@@ -16,58 +17,60 @@ export const TechnoPage = () => {
     techno = decodeURIComponent(techno)
 
     return (
-        <div>
-            <h1>{capitalize(techno)}</h1>
-            <Link to={`/`}>Show all categories</Link><br />
-            <Link to={`/${type}`}>Show all {plural(type)}</Link>
-            <Query query={INTRO_TYPE(type)}>
-                {({ loading, error, data }) => {
-                    if (loading) return 'Loading...'
-                    if (error) return 'Unable to connect to GraphQL api : ' + error.message
-                    const fields: any[] = data.__type.fields
-                    // Isolate interesting fields and map to graphql request
-                    let parameters = fields
-                        .filter(f => !(f.name as string).endsWith('Aggregate'))
-                        .map(f => {
-                            if (f.name.includes('enum') || (!f.type.ofType && f.type.kind != 'OBJECT') || (f.type.ofType && f.type.ofType.name == 'String'))
-                                return f.name
-                            return `${f.name} { name }`
-                        })
-                    return (
-                        <Query query={GET(type, parameters, techno)}>
-                            {({ loading, error, data }) => {
-                                if (loading) return 'Loading...'
-                                if (error) return 'Unable to connect to GraphQL api : ' + error.message
-                                const items = data["get" + capitalize(type)]
-                                console.log(items)
-                                let liItems = Object.keys(items)
-                                    .filter(k => k != '__typename' && items[k] != undefined && !k.includes('github'))
-                                    .map(key =>
-                                        <li key={key}>
-                                            <b>{wellFormated(key)} :</b> <Field fields={fields} name={key} val={items[key]} />
-                                        </li>
+        <section>
+            <div>
+                <h1>{capitalize(techno)}</h1>
+                <Link to={`/`}>Show all categories</Link><br />
+                <Link to={`/${type}`}>Show all {plural(type)}</Link>
+                <Query query={INTRO_TYPE(type)}>
+                    {({ loading, error, data }) => {
+                        if (loading) return 'Loading...'
+                        if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                        const fields: any[] = data.__type.fields
+                        // Isolate interesting fields and map to graphql request
+                        let parameters = fields
+                            .filter(f => !(f.name as string).endsWith('Aggregate'))
+                            .map(f => {
+                                if (f.name.includes('enum') || (!f.type.ofType && f.type.kind != 'OBJECT') || (f.type.ofType && f.type.ofType.name == 'String'))
+                                    return f.name
+                                return `${f.name} { name }`
+                            })
+                        return (
+                            <Query query={GET(type, parameters, techno)}>
+                                {({ loading, error, data }) => {
+                                    if (loading) return 'Loading...'
+                                    if (error) return 'Unable to connect to GraphQL api : ' + error.message
+                                    const items = data["get" + capitalize(type)]
+                                    console.log(items)
+                                    let liItems = Object.keys(items)
+                                        .filter(k => k != '__typename' && items[k] != undefined && !k.includes('github'))
+                                        .map(key =>
+                                            <li key={key}>
+                                                <b>{wellFormated(key)} :</b> <Field fields={fields} name={key} val={items[key]} />
+                                            </li>
+                                        )
+                                    return (
+                                        <div>
+                                            <ul>{liItems}</ul>
+                                            {type == 'dbms' && <a href={`https://dbdb.io/db/${techno}`} target="_blank">View on dbdb.io</a>}
+                                            <Github items={items} />
+                                        </div>
                                     )
-                                return (
-                                    <div>
-                                        <ul>{liItems}</ul>
-                                        {type == 'dbms' && <a href={`https://dbdb.io/db/${techno}`} target="_blank">View on dbdb.io</a>}
-                                        <Github items={items} />
-                                    </div>
-                                )
-                            }}
-                        </Query>
-                    )
-                }}
-            </Query>
-            <div id="widget">
+                                }}
+                            </Query>
+                        )
+                    }}
+                </Query>
+                <div id="widget">
 
-                <GoogleTrends
-                    type="TIMESERIES"
-                    keyword={techno}
-                    url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
-                />
-            </div>
-            <p>See any mistake ? <a href={`https://github.com/stackdb-info/db/blob/master/database/${type}/${techno.replaceAll(' ', '_').toLowerCase()}.yml`}>Edit source</a></p>
-        </div >
+                    <GoogleTrends
+                        type="TIMESERIES"
+                        keyword={techno}
+                        url="https://ssl.gstatic.com/trends_nrtr/2051_RC11/embed_loader.js"
+                    />
+                </div>
+                <p>See any mistake ? <a href={`https://github.com/stackdb-info/db/blob/master/database/${type}/${techno.replaceAll(' ', '_').toLowerCase()}.yml`}>Edit source</a></p>
+            </div >
+        </section>
     )
 }
